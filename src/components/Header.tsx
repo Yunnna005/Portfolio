79d7bbcb5a2abc2d5f9cbdaceeb74777da220992
Yunnna005 <annakovalenko0500@gmail.com>
2025-02-27 13:18:35 +0000

    Header done

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="name">Anna Kovalenko</div>
+        <div className="nav-links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
